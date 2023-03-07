ab415cf20da9a1862916ba48406a490c726d3b92
RaFasT926 <ortizher.926@gmail.com>
2023-03-07 09:14:09 -0500

    Task 02

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,14 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+ingredients.forEach(ingredient => {
+
+const li = document.createElement("li");
+const liText = document.createTextNode(ingredient);
+li.appendChild(liText);
+document.getElementById("ingredients").appendChild(li);
+li.className = "item";
+
+});
