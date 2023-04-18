c1a9b19aba973caf9ffe8b17a9709a20a4951ed1
RaFasT926 <ortizher.926@gmail.com>
2023-04-18 15:37:41 -0500

    Task 08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
 
+const form = document.querySelector('.login-form');
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const emailInput = form.elements.email;
+    const passwordInput = form.elements.password;
+
+    if (emailInput.value === "" || passwordInput.value === "") {
+      alert('Por favor diligencia todos los campos');
+      return;
+    }
+
+    const data = {
+      [emailInput.name]: emailInput.value,
+      [passwordInput.name]: passwordInput.value,
+    };
+    console.log(data);
+
+    form.reset();
+  });
