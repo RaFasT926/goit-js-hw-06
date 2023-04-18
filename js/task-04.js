710a443c3471e64c89df550e89c3afe154d8eb62
RaFasT926 <ortizher.926@gmail.com>
2023-04-18 10:46:49 -0500

    Task 04

---
+++ js/task-04.js
@@ -0,0 +1,16 @@
+const counter =  document.getElementById("counter");
+const valueSpan = document.getElementById("value");
+const incrementButton = counter.querySelector('[data-action="increment"]');
+const decrementButton = counter.querySelector('[data-action="decrement"]');
+
+let counterValue = 0;
+
+incrementButton.addEventListener('click', () => {
+    counterValue++;
+    valueSpan.textContent = counterValue;
+  });
+  
+  decrementButton.addEventListener('click', () => {
+    counterValue--;
+    valueSpan.textContent = counterValue;
+  });
